73205487a058c73711b65882f3b70543c5070077
Olivia Barrington <barrington.olivia@gmail.com>
2017-10-30 17:10:12 -0500

    Import screenSelection

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import App from './App';
+import ScreenSelection from './ScreenSelection';
 import { StackNavigator } from 'react-navigation';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const navigation = { navigate: jest.fn() };
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
