d16167c2c1eed1511ac8e5ce8f701048061e1cec
tdanielsen <tdanielsen@wisc.edu>
2017-10-29 19:05:30 -0500

    Got rid of a bunch of console log stuff

--- App.js
+++ App.js
@@ -3,12 +3,6 @@ import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
 const fireboi = require('./fireboi');
 
-// console.log("test");
-// //var promptee = fireboi.getMeAPrompt('draw');
-// var prompt = fireboi.getMeAPrompt('draw');
-// console.log(prompt);
-//console.log(promptee);
-
 var prompt = "";
 
 export default class App extends React.Component {
@@ -20,51 +14,15 @@ export default class App extends React.Component {
     };
   }
 
-  // setStateAsync(state) {
-  //   return new Promise((resolve) => {
-  //     this.setState(state, resolve)
-  //   });
-  // }
 
   componentWillMount() {
-    prompt = "I'mma failure";
-    console.log("Before:");
-    console.log(prompt);
-    //prompt = fireboi.getMeAPrompt('draw');
     fireboi.getMeAPrompt('draw').then(p => {
       prompt = p;
-      console.log("Prompt generator gives me:")
-      console.log(prompt);
       this.setState({ promptee: prompt });
     });
-    // console.log("After:");
-    // console.log(prompt);
   }
 
-  // shouldComponentUpdate() {
-  //   if (prompt === "") {
-  //     return true;
-  //   }
-  //   return false;
-  // }
-  //
-  // componentWillUpdate() {
-  //   this.setState({ promptee: prompt});
-  // }
-
-  // async componentDidMount() {
-  //   const promptly = await fireboi.getMeAPrompt('draw');
-  //   console.log("I'm a b a d b o y e and didn't let the await function finish");
-  //   console.log(promptly);
-  //   await this.setStateAsync({promptee: promptly});
-  // }
-
   render() {
-    // console.log(promptee);
-    // if (promptee == "") {
-    //   console.log("Rude b o y e");
-    // }
-    //this.setState({ promptee: prompt});
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
--- fireboi.js
+++ fireboi.js
@@ -28,51 +28,40 @@ function getRef() {
   return firebaseApp.database().ref();
 }
 async function selectWord(base, order, type) {
-  console.log("I made in the selectWord function");
   try {
     const word = await genWord(type, order);
-    console.log("The word I got was:");
-    console.log(word);
     return word;
   } catch (error) {
     console.log(error.message);
   }
 }
 function genPrompt(type) {
-  console.log("I made in the genPrompt function");
   return new Promise(
     (resolve, reject) => {
-    console.log("I'm in the promise");
     var baseRef = firebaseApp.database().ref("Bases");
     var base = "";
     var order = "";
     baseRef.orderByChild("type").equalTo(type).once("value").then(function(snapshot) {
       var correctBaseType = [];
-      console.log("I'm in the orderByChild");
       snapshot.forEach(function(child) {
         correctBaseType.push({
           base: child.val().prompt,
           order: child.val().order
         });
       });
-      console.log(base);
-      console.log(order);
       var rand = Math.floor(Math.random()*correctBaseType.length);
       base = correctBaseType[rand].base;
       order = correctBaseType[rand].order;
       var word = "";
       for (var i = 0; i<order.length; i++) {
         word = ""
-        console.log(order[i])
         if (order[i] == "*") {
           selectWord(base, order, "noun").then(word => {
             base = base.replace("*", word);
-            console.log(base);
           });
         } else {
           selectWord(base, order, "verb").then(word => {
             base = base.replace("^", word);
-            console.log(base);
           });
         }
       }
@@ -91,11 +80,9 @@ function genWord(type, order) {
       var word = "";
       //itemsRef.once("value").then(function(snap) {
       for (var i = 0; i < order.length; i++) {
-        console.log(type)
         itemsRef.orderByChild("type").equalTo(type).once('value').then(function(child2) {
           var correctType = [];
           child2.forEach(function (rabbitHole) {
-            console.log(rabbitHole.val().word);
             correctType.push(rabbitHole.val().word);
           });
           var rand = Math.floor(Math.random()*correctType.length);
@@ -114,9 +101,7 @@ function genWord(type, order) {
 
 async function getPrompt(type) {
   try {
-    console.log("I made in the getPrompt function");
     const promptBoi = await genPrompt(type);
-    console.log("I'm a g o o d b o y e and I waited for the await to finish");
     return promptBoi;
   } catch (error) {
     console.log(error.message);
@@ -125,13 +110,6 @@ async function getPrompt(type) {
 
 module.exports = {
   getMeAPrompt: function(type) {
-    console.log("I made in the export function");
-    // var prompt = "";
-    // getPrompt('draw').then(p => {
-    //   prompt = p;
-    //   console.log("Prompt generator gives me:")
-    //   console.log(prompt);
-    // });
     return getPrompt(type);
   }
 }
