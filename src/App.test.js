a76453196ba2ee5dc63054eb09b9d91c5669126c
obarrington <barrington.olivia@gmail.com>
2017-10-30 13:16:27 -0500

    updating app.test.js
    build is failing i wanna fix it

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ import { StackNavigator } from 'react-navigation';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
+  const navigation = { navigate: jest.fn() };
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
