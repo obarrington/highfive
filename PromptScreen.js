78c255f51e3a9cb2c50ab3d13d822004065bfecf
hendricksong <32659933+hendricksong@users.noreply.github.com>
2017-10-30 21:59:25 -0500

    Completed Prompt Screen
    Draw and Write buttons now take user to prompt screen, where a randomly generated prompt is displayed

---
+++ PromptScreen.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Container from './Container';
+import Button from './Button';
+import App from './App';
+import ScreenSelection from './ScreenSelection'
+
+const database = require('./database');
+
+var exercisePrompt = "";
+
+export default class PromptScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prompt: "loading..."
+    };
+    this.promptType = this.props.navigation.state.params.type;
+  }
+
+  componentWillMount() {
+    if(this.promptType == "draw") {
+      this.setState({style: styles.drawContainer});
+    }
+    else {
+      this.setState({style: styles.writeContainer})
+    }
+    database.getMeAPrompt(this.promptType).then(p => {
+      exercisePrompt = p;
+      this.setState({ prompt: exercisePrompt });
+    });
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={this.state.style}>
+        <Text style={styles.label}>{this.state.prompt}</Text>
+        </View>
+      </View>
+    );
+  };
+}
+const styles = StyleSheet.create({
+  writeContainer: {
+    flex: 1,
+    backgroundColor: '#75c68b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  drawContainer: {
+    flex: 1,
+    backgroundColor: '#34A853',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Verdana',
+    color: '#fff',
+  },
+  primaryButton: {
+    backgroundColor: 'transparent',
+  },
+});
---
+++ ScreenSelection.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Container from './Container';
+import Button from './Button';
+import App from './App';
+
+
+export default class ScreenSelection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+
+    this.write=this.write.bind(this);
+    this.draw=this.draw.bind(this);
+  }
+
+  write() {
+      const { navigate } = this.props.navigation;
+
+      navigate('Prompt', {type: "write"});
+  };
+
+  draw() {
+      const { navigate } = this.props.navigation;
+
+      navigate('Prompt', {type: "draw"});
+  };
+
+
+
+
+  render() {
+    return (
+    <View style={{flex: 1}}>
+      <View style={styles.writeContainer}>
+      <Button
+        label="Write"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.write}
+        />
+      </View>
+      <View style={styles.drawContainer}>
+      <Button
+        label="Draw"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.draw}
+        />
+
+      </View>
+    </View>
+  );
+
+};
+}
+  const styles = StyleSheet.create({
+    writeContainer: {
+      flex: 1,
+      backgroundColor: '#75c68b',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    drawContainer: {
+      flex: 1,
+      backgroundColor: '#34A853',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    label: {
+      fontSize: 50,
+      fontWeight: 'bold',
+      fontFamily: 'Verdana',
+      color: '#fff',
+    },
+    primaryButton: {
+      backgroundColor: 'transparent',
+    },
+});
