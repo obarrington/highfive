d43820ba5ad75be46af71576b1ee3ea771b85095
hendricksong <hendricksong@wisc.edu>
2017-11-01 16:43:53 -0500

    Added Write Screen
    added basic write screen functionality

--- src/WriteScreen.js
+++ src/WriteScreen.js
@@ -1,26 +1,67 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import Container from './Container';
-import Button from './Button';
-import App from './App';
+/**
+ * Created by JunjiJoo on 2017-10-31.
+ */
+import React from 'react';
+import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
 
 
-export default class ScreenSelection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <Text>Write screen Placeholder</Text>
-      </View>
-  );
-
-};
+export default class Write extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {text: 'Write Answer to the Prompt',};
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style = {styles.containerA}>
+                    <Text style = {styles.headline}>Prompt Goes Here</Text>
+                </View>
+                <View style = {styles.containerB}>
+                    <TextInput
+                        style = {styles.textInput}
+                        onChangeText = {(text) => this.setState({text})}
+                        value = {this.state.text}
+                    />
+                </View>
+            </View>
+        );
+    }
 }
-  const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#8fbc8f',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerA: {
+        flex: 1,
+        backgroundColor: '#8fbc8f',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerB: {
+        flex: 3,
+        backgroundColor: '#8fbc8f',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        height: 300,
+        width: 250,
+        borderColor: 'gray',
+        borderWidth: 1,
+        alignSelf: 'center',
+    },
+    headline: {
+        fontSize: 20,
+        textAlign: 'center',
+        backgroundColor: 'rgba(0,0,0,0)',
+        color: 'black',
+    }
+
 
 });
+
+AppRegistry.registerComponent('AwesomeProject', () => App);
