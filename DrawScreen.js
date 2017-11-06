eac77823a9fe02ec99eaadc762a1e27540c02c3c
Jackie Roush <kroush@wisc.edu>
2017-11-05 18:59:34 -0600

    starting new branch. this kills all

--- DrawScreen.js
+++ DrawScreen.js
@@ -4,6 +4,34 @@ import { StackNavigator } from 'react-navigation';
 import Container from './Container';
 import Button from './Button';
 import App from './App';
+
+const ReactDOM = require('react-dom');
+const LC = require('literallycanvas');
+
+
+
+export default class DrawScreen extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {};
+  }
+    
+    render() {
+        return (<LC.LiterallyCanvasReactComponent imageURLPrefix="/build/lc-assets/img" />);
+    }
+};
+
+
+//ReactDOM.render(<DrawScreen />, document.getElementById('litcanvasID'));
+
+
+/*
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Container from './Container';
+import Button from './Button';
+import App from './App';
 //This is straight from literallycanvas. should produce the basic canvas
 //const React = require('react');
 const ReactDOM = require('react-dom');
@@ -20,6 +48,7 @@ const MyApp = React.createClass({
 ReactDOM.render(<MyApp />, document.getElementById('main'));
 
 
+*/
 
 
 /*
