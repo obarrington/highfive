fb4ad0dc2460cdcb0b8cf88fcedfa3d87481aad0
yjoo7 <yjoo7@wisc.edu>
2017-11-26 16:06:44 -0600

    before making 2nd branch for end screen
    can't find variable "write" error exist and the expo app won't open

--- EndScreen.js
+++ EndScreen.js
@@ -10,17 +10,80 @@ export default class EndScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+
+        this.onButtonPress=this.onButtonPress.bind(this)
+    }
+
+    onButtonPress() {
+        const { navigate } = this.props.react-navigation
+
+        navigate('Prompt');
     }
 
     render() {
         return (
             <View style={{flex: 1}}>
-    <Text>End screen Placeholder</Text>
+    <View style={styles.continueContainer}>
+    <Button
+        label="Continue Working"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.onButtonPress}
+    />
+    </View>
+        <View style={styles.playAgainContainer}
+    <Button
+        label="Play Again"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.onButtonPress}
+    />
+    </View>
+        <View style={styles.saveContainer}
+    <Button
+        label="Save Work"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.onButtonPress}
+    />
+    </View>
+        <View style={styles.shareContainer}
+    <Button
+        label="Share Work"
+        styles={{button: styles.primaryButton, label:styles.label}}
+        onPress={this.onButtonPress}
+    />
+    </View>
         </View>
     );
-
     };
 }
 const styles = StyleSheet.create({
-
+    continueContainer: {
+        flex: 1,
+        backgroundColor: '#75c68b',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    shareContainer: {
+        flex: 1,
+        backgroundColor: '#75c68b',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    saveContainer: {
+        flex: 1,
+        backgroundColor: '#75c68b',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    playAgainContainer: {
+        flex: 1,
+        backgroundColor: '#75c68b',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    label: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontFamily: 'Verdana',
+        color: '#fff',
+    }
 });
